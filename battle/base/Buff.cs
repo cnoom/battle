44149795e7baa73833e0d951e683e8deaedba4a7
WIN-U816FO4SA1Q\Administrator <1195582817@qq.com>
2023-05-19 16:18:08 +0800

    基础伤害类型设定

---
+++ battle/base/Buff.cs
@@ -0,0 +1,19 @@
+﻿namespace battle;
+
+public struct Buff
+{
+    /**
+     * 毒
+     */
+    public int poison;
+
+    /**
+     * 火
+     */
+    public int fire;
+    
+    /**
+     * 冰
+     */
+    public int ice
+}
---
+++ battle/base/Damage.cs
@@ -0,0 +1,17 @@
+﻿namespace battle;
+
+public struct Damage
+{
+    /**
+     * 物理伤害
+     */
+    public int physical;
+    /**
+     * 真实伤害
+     */
+    public int real;
+    /**
+     * buff
+     */
+    public Buff buff;
+}
